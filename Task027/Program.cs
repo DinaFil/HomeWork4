9902f622993d48b893f254d48dc748ace8d5d538
Dina Filippova <dinafilippova@gmail.com>
2022-12-11 17:47:15 +0700

    Сумма цифр числа

---
+++ Task027/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+int result = 0;
+while (number > 0)
+{
+result = result + number % 10;
+number = number /10;
+}
+Console.WriteLine(result);
